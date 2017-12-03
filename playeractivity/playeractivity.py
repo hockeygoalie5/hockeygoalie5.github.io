962a654d6c1e8ef5094f0779bd77dd39d64e848d
Mustafa Kalash <me@mkalash.com>
2017-12-03 06:27:25 -0500

    Player activity updates.

--- playeractivity/playeractivity.py
+++ playeractivity/playeractivity.py
@@ -3,75 +3,79 @@ from glob import glob as getFiles
 import datetime
 import json
 import os.path
-
-skippedDays = {}
-hoursPlayed = {}
+import codecs
 
 if os.path.isfile("playeractivity.json"):
     with open("playeractivity.json", "r") as f:
         jsonDump = json.loads(f.read())
     jsonDump["generatedOn"] = datetime.datetime.today().strftime("%Y-%m-%d %H:%M:%S")
-    for player in jsonDump["players"]:
-        skippedDays[player["key"]] = player["skippedDays"]
-        hoursPlayed[player["key"]] = player["hoursPlayed"]
-    jsonDump["players"] = list()
 else:
-    jsonDump = {"generatedOn": datetime.datetime.today().strftime("%Y-%m-%d %H:%M:%S"), "numDays": 0, "dates": list(), "players": list()}
+    jsonDump = {"generatedOn": datetime.datetime.today().strftime("%Y-%m-%d %H:%M:%S"), "days": dict()}
 
 for filename in getFiles("logs/*/*/*.log"):
-    pathSplit = filename.split("\\")
-    date = pathSplit[1] + "/" + pathSplit[2] + "/" + pathSplit[3][:-4]
-    if date in jsonDump["dates"]:
+    pathSplit = filename.split("/")
+    date = pathSplit[1] + "-" + pathSplit[2].split("-")[0] + "-" + pathSplit[3].split("-")[0]
+
+    if date in jsonDump["days"]:
         continue
+
+    jsonDump["days"][date] = dict()
+
     print("---- %s ----" % date)
+
     logs = ""
-    jsonDump["numDays"] += 1
-    jsonDump["dates"].append(date)
-    with open(filename) as f:
+    with codecs.open(filename, "r", "latin1") as f:
         logs = f.read()
-    findKeys = re.compile("(?<=Login: )\w*?(?=\/)")
+
+    findKeys = re.compile(r"(?<=Login: ).*?(?=\/)", re.IGNORECASE)
     keys = set(key.lower() for key in re.findall(findKeys, logs))
-    for key in keys:
+    totalSkips = 0
+    for i, key in enumerate(keys):
         loginTimes = list()
         logoutTimes = list()
         mobNames = set()
-        if key not in hoursPlayed:
-            hoursPlayed[key] = 0
-        if key not in skippedDays:
-            skippedDays[key] = 0
+
+        jsonDump["days"][date][key] = {"secondsPlayed": 0, "active": 0, "skipped": 0}
+
+        findActive = re.compile(r"(EMOTE|SAY): (?!\(DSAY\)).*?/\(?(%s) :" % re.escape(key), re.IGNORECASE)
+        if re.search(findActive, logs) != None:
+            jsonDump["days"][date][key]["active"] = 1
+
         findLoginTimes = re.compile(r"\[(\d\d:\d\d:\d\d)\].*Login: %s\/\((.*)\)" % re.escape(key), re.IGNORECASE)
         for match in re.findall(findLoginTimes, logs):
             loginTimes.append(datetime.datetime.strptime(match[0], "%H:%M:%S"))
             mobNames.add(match[1])
+
         mobNameList = ""
-        i = 0
-        for mobName in mobNames:
+        for j, mobName in enumerate(mobNames):
+            if mobName.lower() == "new player":
+                continue
             mobNameList += re.escape(mobName)
-            if(i < len(mobNames)):
+            if(j < len(mobNames) - 1):
                 mobNameList += "|"
-            i += 1
+
         findLogoutTimes = re.compile(r"\[(\d\d:\d\d:\d\d)\].*Logout: (%s|\*no key\*\/\((%s|%s)\))" % (re.escape(key), re.escape(key), mobNameList), re.IGNORECASE)
         for match in re.findall(findLogoutTimes, logs):
             logoutTimes.append(datetime.datetime.strptime(match[0], "%H:%M:%S"))
+
         timeDelta = datetime.timedelta()
-        if len(loginTimes) < len(logoutTimes):
-            print("-- SKIPPING %s DUE TO NON-UNIQUE MOB NAME --" % key)
-            skippedDays[key] += 1
-            continue
-        if len(loginTimes) > len(logoutTimes):
-            print("-- SKIPPING %s DUE TO MISSING LOGOUT LOG --" % key)
-            skippedDays[key] += 1
-            continue
-        for i in range(0, len(loginTimes)):
-            dt = logoutTimes[i] - loginTimes[i]
-            if dt.seconds < 0:
-                print("-- SKIPPING %s DUE TO ERROR IN LOG TIMESTAP --" % key)
-                skippedDays[key] += 1
-            timeDelta += dt
-        hoursPlayed[key] += timeDelta.seconds / 60 / 60
-for key in hoursPlayed:
-    daysSkipped = skippedDays[key]
-    player = {"key": key, "hoursPlayed": hoursPlayed[key], "skippedDays": daysSkipped}
-    jsonDump["players"].append(player)
-with open("playeractivity.json", "w") as f:
-    f.write(json.dumps(jsonDump))
+        doSkip = len(loginTimes) < len(logoutTimes) or len(loginTimes) > len(logoutTimes)
+        if not doSkip:
+            for j in range(0, len(loginTimes)):
+                dt = logoutTimes[j] - loginTimes[j]
+                if dt.seconds < 0:
+                    doSkip = True
+                timeDelta += dt
+        if doSkip:
+            jsonDump["days"][date][key]["skipped"] = 1
+            totalSkips += 1
+        else:
+            jsonDump["days"][date][key]["secondsPlayed"] = timeDelta.seconds
+
+        print("\rScanned %i of %i players with %i errors." % (i + 1, len(keys), totalSkips), end = "")
+    print("\n")
+
+    with open("playeractivity.json", "w") as f:
+        f.write(json.dumps(jsonDump))
+
+print("Done.")
--- playeractivity/scripts.js
+++ playeractivity/scripts.js
@@ -8,20 +8,17 @@ const staff = [
   "mkalash",
   "xales",
   "spookerton",
-  "loneguyfly",
   "chronograph",
   "paradoxon",
   "masterrbc",
   "crushtoe",
   "ace mclazer",
   "pobiega",
-  "serveris6",
   "ftangsteve",
   "psiomegadelta",
   "techhead0",
   "mordeth221",
   "shadow_of_man",
-  "cirra",
   "eckles_fire",
   "graciegrace0",
   "huntime",
@@ -32,10 +29,13 @@ const staff = [
   "katastrofa",
   "commissardrew",
   "levyafan",
-  "noblecaos"
+  "noblecaos",
+  "aetsuki",
+  "earthcrusher",
+  "chinsky"
 ];
 
-function buildData() {
+function buildData(setDates) {
   var xobj = new XMLHttpRequest();
   xobj.overrideMimeType("application/json");
   xobj.open("GET", "playeractivity.json", true);
@@ -43,13 +43,39 @@ function buildData() {
     if (xobj.readyState == 4 && xobj.status == "200") {
       var playerActivity = JSON.parse(xobj.responseText);
 
+      var dates = Object.keys(playerActivity["days"]).sort(function(a,b) {
+          return Date.parse(a) > Date.parse(b);
+      });
+
+      var minDate = dates[0];
+      var maxDate = dates[dates.length - 1]
+      
+      var fromDateInput = document.getElementById("fromDate");
+      var toDateInput = document.getElementById("toDate");
+
+      fromDateInput.min = minDate;
+      fromDateInput.max = maxDate;
+      toDateInput.min = minDate;
+      toDateInput.max = maxDate;
+
+      if(setDates) {
+        fromDateInput.value = minDate;
+        toDateInput.value = maxDate;
+      }
+
+      var fromDate = new Date(fromDateInput.value);
+      var toDate = new Date(toDateInput.value);
+
       var shouldAverage = document.getElementById("radioAveraged").checked;
       var filterStaff = document.getElementById("checkStaffOnly").checked;
 
+      var searchString = document.getElementById("search").value.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
+
       document.getElementById("generatedDate").innerText = "Generated on " + playerActivity["generatedOn"] + " EST";
 
-      var totalDays = playerActivity["numDays"];
-      document.getElementById("daysAnalyzed").innerText = "Data gathered over " + totalDays + " days.";
+      var totalDays = ((toDate.getTime() - fromDate.getTime()) / 86400000) + 1;
+
+      document.getElementById("daysAnalyzed").innerText = "Range includes " + totalDays + " days.";
 
       if(shouldAverage) {
         document.getElementById("totalNotice").hidden = true;
@@ -62,20 +88,42 @@ function buildData() {
         dataTableBody.removeChild(dataTableBody.firstChild);
       }
 
-      playerActivity["players"].sort(function(a, b) {
-        var aAverageHoursPlayed = a["hoursPlayed"] / (totalDays - a["skippedDays"]);
-        var bAverageHoursPlayed = b["hoursPlayed"] / (totalDays - b["skippedDays"]);
-        if(shouldAverage) {
-          return aAverageHoursPlayed < bAverageHoursPlayed;
+      dates = dates.filter(date => (fromDate.getTime() <= Date.parse(date) && toDate.getTime() >= Date.parse(date)));
+
+      var players = [];
+      for(date of dates) {
+        for(player in playerActivity["days"][date]) {
+          var playerIndex = players.findIndex(function(p) {
+            return p["key"] === player;
+          });
+          if(playerIndex > -1) {
+            players[playerIndex]["activeDays"] += playerActivity["days"][date][player]["active"];
+
+            if(playerActivity["days"][date][player]["skipped"]) {
+              players[playerIndex]["skippedDays"] += 1;
+            } else {
+              players[playerIndex]["secondsPlayed"] += playerActivity["days"][date][player]["secondsPlayed"];
+            }
+          } else {
+            players.push({"key": player, "secondsPlayed": playerActivity["days"][date][player]["secondsPlayed"], "activeDays": playerActivity["days"][date][player]["active"], "skippedDays": playerActivity["days"][date][player]["skipped"]});
+          }
         }
-        var aSortNumber = a["hoursPlayed"];
-        var bSortNumber = b["hoursPlayed"];
+      }
+
+      players.sort(function(a, b) {
+        var aAverageSecondsPlayed = a["secondsPlayed"] / (totalDays - a["skippedDays"]);
+        var bAverageSecondsPlayed = b["secondsPlayed"] / (totalDays - b["skippedDays"]);
+        if(shouldAverage) {
+          return aAverageSecondsPlayed < bAverageSecondsPlayed;
+        }
+        var aSortNumber = a["secondsPlayed"];
+        var bSortNumber = b["secondsPlayed"];
 
         if(a["skippedDays"]) {
-          aSortNumber = aSortNumber + (aAverageHoursPlayed * a["skippedDays"]);
+          aSortNumber = aSortNumber + (aAverageSecondsPlayed * a["skippedDays"]);
         }
         if(b["skippedDays"]) {
-          bSortNumber = bSortNumber + (bAverageHoursPlayed * b["skippedDays"]);
+          bSortNumber = bSortNumber + (bAverageSecondsPlayed * b["skippedDays"]);
         }
 
         return aSortNumber < bSortNumber;
@@ -83,7 +131,7 @@ function buildData() {
 
       var displayedPlayers = 0;
       var activeStaff = [];
-      for(player of playerActivity["players"]) {
+      for(player of players) {
         if(filterStaff) {
           if(staff.indexOf(player["key"]) == -1) {
             continue;
@@ -93,31 +141,52 @@ function buildData() {
 
         displayedPlayers++;
 
+        if(player["key"].search(searchString) < 0) {
+          continue;
+        }
+
         var newRow = document.createElement("tr");
         var rankCell = document.createElement("td");
         var keyCell = document.createElement("td");
         var hoursPlayedCell = document.createElement("td");
+        var activeDaysCell = document.createElement("td");
         var skippedDaysCell = document.createElement("td");
 
         rankCell.innerText = displayedPlayers;
         keyCell.innerText = player["key"];
-        var hoursPlayed = player["hoursPlayed"];
-        var averageHoursPlayed = hoursPlayed / (totalDays - player["skippedDays"]);
+        var secondsPlayed = player["secondsPlayed"];
+        var averageSecondsPlayed = secondsPlayed / (totalDays - player["skippedDays"]);
+
         if(shouldAverage) {
-          hoursPlayed = averageHoursPlayed;
-        } 
-        if(!shouldAverage && player["skippedDays"]) {
-          var estimatedTotalHours = hoursPlayed + (averageHoursPlayed * player["skippedDays"]);
-          hoursPlayedCell.innerText = Math.floor(estimatedTotalHours) + ":" + Math.floor((estimatedTotalHours * 60) % 60).toLocaleString(undefined, {minimumIntegerDigits: 2, useGrouping:false}) + ":" + ((((estimatedTotalHours * 60) % 60) * 60) % 60).toLocaleString(undefined, {minimumIntegerDigits: 2, maximumFractionDigits: 0, useGrouping:false});
-          hoursPlayedCell.innerText += " (" + Math.floor(hoursPlayed) + ":" + Math.floor((hoursPlayed * 60) % 60).toLocaleString(undefined, {minimumIntegerDigits: 2, useGrouping:false}) + ":" + ((((hoursPlayed * 60) % 60) * 60) % 60).toLocaleString(undefined, {minimumIntegerDigits: 2, maximumFractionDigits: 0, useGrouping:false}) + ")";
+          var hours = Math.floor(averageSecondsPlayed / 3600);
+          averageSecondsPlayed %= 3600;
+          var minutes = Math.floor(averageSecondsPlayed / 60);
+          averageSecondsPlayed = Math.floor(averageSecondsPlayed % 60);
+          hoursPlayedCell.innerText = hours.toLocaleString(undefined, {minimumIntegerDigits: 2, useGrouping:false}) + ":" + minutes.toLocaleString(undefined, {minimumIntegerDigits: 2, useGrouping:false}) + ":" + averageSecondsPlayed.toLocaleString(undefined, {minimumIntegerDigits: 2, useGrouping:false});
         } else {
-          hoursPlayedCell.innerText = Math.floor(hoursPlayed) + ":" + Math.floor((hoursPlayed * 60) % 60).toLocaleString(undefined, {minimumIntegerDigits: 2, useGrouping:false}) + ":" + ((((hoursPlayed * 60) % 60) * 60) % 60).toLocaleString(undefined, {minimumIntegerDigits: 2, maximumFractionDigits: 0, useGrouping:false});
+          var estimatedTotalSeconds = secondsPlayed + (averageSecondsPlayed * player["skippedDays"]);
+
+          var hours = Math.floor(estimatedTotalSeconds / 3600);
+          estimatedTotalSeconds %= 3600;
+          var minutes = Math.floor(estimatedTotalSeconds / 60);
+          estimatedTotalSeconds = Math.floor(estimatedTotalSeconds % 60);
+          hoursPlayedCell.innerText = hours.toLocaleString(undefined, {minimumIntegerDigits: 2, useGrouping:false}) + ":" + minutes.toLocaleString(undefined, {minimumIntegerDigits: 2, useGrouping:false}) + ":" + estimatedTotalSeconds.toLocaleString(undefined, {minimumIntegerDigits: 2, useGrouping:false});
+          
+          if(player["skippedDays"]) {
+            hours = Math.floor(secondsPlayed / 3600);
+            secondsPlayed %= 3600;
+            minutes = Math.floor(secondsPlayed / 60);
+            secondsPlayed = Math.floor(secondsPlayed % 60);
+            hoursPlayedCell.innerText += " (" + hours.toLocaleString(undefined, {minimumIntegerDigits: 2, useGrouping:false}) + ":" + minutes.toLocaleString(undefined, {minimumIntegerDigits: 2, useGrouping:false}) + ":" + secondsPlayed.toLocaleString(undefined, {minimumIntegerDigits: 2, useGrouping:false}) + ")";
+          }
         }
         skippedDaysCell.innerText = player["skippedDays"];
+        activeDaysCell.innerHTML = player["activeDays"];
 
         newRow.appendChild(rankCell);
         newRow.appendChild(keyCell);
         newRow.appendChild(hoursPlayedCell);
+        newRow.appendChild(activeDaysCell);
         newRow.appendChild(skippedDaysCell);
 
         dataTableBody.appendChild(newRow);
@@ -131,20 +200,27 @@ function buildData() {
         for(staffMember of inactiveStaff) {
           displayedPlayers++;
 
+          if(staffMember.search(searchString) < 0) {
+            continue;
+          }
+
           var newRow = document.createElement("tr");
           var rankCell = document.createElement("td");
           var keyCell = document.createElement("td");
           var hoursPlayedCell = document.createElement("td");
+          var activeDaysCell = document.createElement("td");
           var skippedDaysCell = document.createElement("td");
 
           rankCell.innerText = displayedPlayers;
           keyCell.innerText = staffMember;
           hoursPlayedCell.innerText = "-------";
+          activeDaysCell.innerText  = "-------"; 
           skippedDaysCell.innerText = "-------";
 
           newRow.appendChild(rankCell);
           newRow.appendChild(keyCell);
           newRow.appendChild(hoursPlayedCell);
+          newRow.appendChild(activeDaysCell);
           newRow.appendChild(skippedDaysCell);
 
           dataTableBody.appendChild(newRow);
@@ -152,14 +228,30 @@ function buildData() {
       }
 
       document.getElementById("rankHeader").innerText = "Rank\n/ " + displayedPlayers;
+      document.getElementById("activeDaysHeader").innerText = "Active days\n/ " + totalDays;
     }
   };
   xobj.send(null);
 }
 
 window.onload = function() {
-    buildData();
-    document.getElementById("radioAveraged").addEventListener("click", buildData)
-    document.getElementById("radioTotal").addEventListener("click", buildData)
-    document.getElementById("checkStaffOnly").addEventListener("click", buildData)
+    buildData(true);
+    document.getElementById("radioAveraged").addEventListener("change", function() {
+      buildData(false);
+    })
+    document.getElementById("radioTotal").addEventListener("change", function() {
+      buildData(false);
+    })
+    document.getElementById("checkStaffOnly").addEventListener("change", function() {
+      buildData(false);
+    })
+    document.getElementById("fromDate").addEventListener("change", function() {
+      buildData(false);
+    });
+    document.getElementById("toDate").addEventListener("change", function() {
+      buildData(false);
+    });
+    document.getElementById("search").addEventListener("input", function() {
+      buildData(false);
+    });
 };
